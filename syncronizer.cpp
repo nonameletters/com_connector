29ffe11df9d6062cb9056a8e6067d8f33cad5276
Митрофанов <vmitrofanov@eatmeat.ru>
2016-06-22 17:08:53 +0300

    add windows service realization

---
+++ syncronizer.cpp
@@ -0,0 +1,7 @@
+#include "syncronizer.h"
+
+CSyncronizer::CSyncronizer()
+{
+
+}
+
---
+++ syncronizer.h
@@ -0,0 +1,11 @@
+#ifndef CSYNCRONIZER_H
+#define CSYNCRONIZER_H
+
+
+class CSyncronizer
+{
+public:
+    CSyncronizer();
+};
+
+#endif // CSYNCRONIZER_H
